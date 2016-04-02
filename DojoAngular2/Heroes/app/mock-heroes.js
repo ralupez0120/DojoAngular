7d71dfd2bb9e6f89facf6f11832a6c1d5336439b
ralupez0120 <ralp2089@gmail.com>
2016-04-02 12:58:36 -0500

    subo lo realizado en el dojos

---
+++ DojoAngular2/Heroes/app/mock-heroes.js
@@ -0,0 +1,64 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Heroes;
+    return {
+        setters:[],
+        execute: function() {
+            './hero';
+            exports_1("Heroes", Heroes = {});
+            {
+                "id";
+                11, "name";
+                "Jesus";
+            }
+            {
+                "id";
+                11, "name";
+                "IronMan";
+            }
+            {
+                "id";
+                11, "name";
+                "SuperMan";
+            }
+            {
+                "id";
+                11, "name";
+                "WonderWoman";
+            }
+            {
+                "id";
+                11, "name";
+                "Flash";
+            }
+            {
+                "id";
+                11, "name";
+                "Spiderman";
+            }
+            {
+                "id";
+                11, "name";
+                "Batman";
+            }
+            {
+                "id";
+                11, "name";
+                "Superman";
+            }
+            {
+                "id";
+                11, "name";
+                "Linterna verde";
+            }
+            ;
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=mock-heroes.js.map
---
+++ DojoAngular2/Heroes/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import {Hero} from: './hero';
+
+export var Heroes:Hero[]={
+	{"id":11, "name":"Jesus"},
+	{"id":11, "name":"IronMan"},
+	{"id":11, "name":"SuperMan"},
+	{"id":11, "name":"WonderWoman"},
+	{"id":11, "name":"Flash"},
+	{"id":11, "name":"Spiderman"},
+	{"id":11, "name":"Batman"},
+	{"id":11, "name":"Superman"},
+	{"id":11, "name":"Linterna verde"},
+};
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
